f071699009b0d61f83dca344442592f533c8d22e
Andyloz <lozandyloz@gmail.com>
2024-06-18 12:01:19 +0200

    Header style done

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -3,18 +3,25 @@ import { FC, ReactNode, SVGProps } from 'react'
 import TableCells from '@/components/icons/table-cells'
 import Users from '@/components/icons/users'
 import BellAlert from '@/components/icons/bell-alert'
+import { clsx } from 'clsx/lite'
 
 export default function Header() {
   return (
     <header className='mb-6 flex flex-wrap justify-between items-center'>
       <h1 className='text-4xl'>Micros</h1>
       <MenuButton />
-      <nav className={`
-        flex-[100%] grid transition-all
-        grid-rows-[0fr] [.open+&]:grid-rows-[1fr]
-        px-4 [.open+&]:py-8
-      `}>
-        <ul className={`overflow-hidden flex flex-wrap gap-3 min-[1px]:flex-col`}>
+      <nav className={clsx(
+        'grid [@media(width<850px)]:flex-[100%]',
+        'ease-in [transition:all_150ms,opacity_500ms]',
+        '[@media(width<850px)]:grid-rows-[0fr] [.open+&]:grid-rows-[1fr]',
+        '[@media(width<850px)]:opacity-0 [.open+&]:opacity-100',
+        '[@media(width<850px)]:px-4 [@media(width<850px)]:[.open+&]:py-8',
+      )}>
+        <ul className={clsx(
+          'overflow-hidden flex gap-3',
+          '[@media(width<550px)]:flex-col',
+          '[@media(550px<=width)]:flex-wrap',
+        )}>
           <li><NavButton Icon={TableCells}>Ver todos los horarios</NavButton></li>
           <li><NavButton Icon={Users}>Lista de hermanos</NavButton></li>
           <li><NavButton Icon={BellAlert}>Gestionar avisos</NavButton></li>
@@ -26,7 +33,7 @@ export default function Header() {
 
 function NavButton({ children, Icon }: { children: ReactNode, Icon: FC<SVGProps<SVGSVGElement>> }) {
   return (
-    <button className='w-full flex items-center rounded border py-2 text-sm hover:bg-gray-50'>
+    <button className='w-full max-w-80 mx-auto flex items-center rounded border py-2 text-sm hover:bg-gray-50'>
       <span className='px-4'><Icon className='size-5' /></span>
       <span className='pe-4 mx-auto'>{children}</span>
     </button>
--- components/header/menu-button.tsx
+++ components/header/menu-button.tsx
@@ -4,7 +4,11 @@ import Bars2 from '@/components/icons/bars-2'
 
 export default function MenuButton() {
   return (
-    <button title='Menu' onClick={(e) => e.currentTarget.classList.toggle('open')}>
+    <button
+      title='Menu'
+      className='[@media(850px<=width)]:hidden'
+      onClick={(e) => e.currentTarget.classList.toggle('open')}
+    >
       <Bars2 className='size-4 my-3 mx-4' />
     </button>
   )
